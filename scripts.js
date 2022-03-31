b7de09cda11a0a1ae96bf1b1e33101ca354d23ab
rmallonce <31581916+rmallonce@users.noreply.github.com>
2022-03-30 20:20:25 -0400

    2nd draft of page

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
-console.log("hello")
+let purchaseNum = document.getElementById("purchaseNum");
+
+let newValue
+
+document.getElementById("upArrow").addEventListener("click", function() {
+
+	newValue = parseInt(purchaseNum.value) + 1;
+
+	console.log(newValue)
+
+	purchaseNum.value = JSON.stringify(newValue);
+
+
+
+})
+
+document.getElementById("downArrow").addEventListener("click", function() {
+
+	newValue = parseInt(purchaseNum.value) - 1;
+
+	console.log(newValue)
+
+	purchaseNum.value = JSON.stringify(newValue);
+
+
+
+})
